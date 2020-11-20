f7a651cb5a285f67e4c69e0725bf1b2ba347e572
unknown <hja5432@gmail.com>
2020-11-20 17:23:17 +0900

    implement login and logout

--- app.js
+++ app.js
@@ -3,22 +3,42 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const sequelize = require('./models').sequelize;
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const breakdownRouter = require('./routes/breakdown');
-const eventRouter = require('/routes/event');
-const guestRouter = require('./routes/guest');
-const organizerRouter  = require('./routes/organizer');
-const app = express();
+const authRouter = require('./routes/auth');
 
-app.all('/*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+// TODO: not use yet 
+// const usersRouter = require('./routes/users');
+// const breakdownRouter = require('./routes/breakdown');
+// const eventRouter = require('./routes/event');
+// const guestRouter = require('./routes/guest');
+// const organizerRouter  = require('./routes/organizer');
+
+
+const app = express();
+sequelize.sync();
+
+app.use(session({
+    key: 'sid',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 //one hour(ms)
+    }
+  })
+);
+
+//TODO : not use yet
+// app.all('/*', function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,11 +47,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/breakdown', breakdownRouter);
-app.use('/event', eventRouter);
-app.use('/guest', guestRouter);
-app.use('./organizer', organizerRouter);
+app.use('/auth', authRouter);
+// app.use('/users', usersRouter);
+// app.use('/breakdown', breakdownRouter);
+// app.use('/event', eventRouter);
+// app.use('/guest', guestRouter);
+// app.use('/organizer', organizerRouter);
 
 app.use(function(req, res, next) {
     next(createError(404));
