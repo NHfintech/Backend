04c10c12f5407582c42188e963dffe23d2536309
jaypae95 <jaypae95@gmail.com>
2020-11-19 23:17:29 -0800

    add routes and settings

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var breakdownRouter = require('./routes/breakdown');
+var eventRouter = require('/routes/event');
+var guestRouter = require('./routes/guest');
+var organizerRouter  = require('./routes/organizer');
 var app = express();
+
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/breakdown', breakdownRouter);
+app.use('/event', eventRouter);
+app.use('/guest', guestRouter);
+app.use('./organizer', organizerRouter);
 
 module.exports = app;
