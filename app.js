b16729ebcdd164fdcd6ebd62e8c6e619f9068846
jaypae95 <jaypae95@gmail.com>
2020-11-19 23:20:05 -0800

    add routes and settings

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var breakdownRouter = require('./routes/breakdown');
-var eventRouter = require('/routes/event');
-var guestRouter = require('./routes/guest');
-var organizerRouter  = require('./routes/organizer');
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const breakdownRouter = require('./routes/breakdown');
+const eventRouter = require('/routes/event');
+const guestRouter = require('./routes/guest');
+const organizerRouter  = require('./routes/organizer');
+const app = express();
 
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -17,7 +18,7 @@ app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
     res.header('Access-Control-Allow-Credentials', true);
     next();
-  });
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,5 +32,19 @@ app.use('/breakdown', breakdownRouter);
 app.use('/event', eventRouter);
 app.use('/guest', guestRouter);
 app.use('./organizer', organizerRouter);
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 module.exports = app;
