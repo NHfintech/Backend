5d966322081c63d7d5b0949ed9ced69d4539eadd
KHS-kr <ks96ks@ajou.ac.kr>
2020-11-21 18:25:41 +0900

    핸드폰번호 중복 확인, 회원가입

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.User.findOne({where : {phone_number : req.query.phone_number}})
+    .then(result =>
+      res.json({
+        message : "이미 가입된 핸드폰 번호가 있습니다.",
+        result : result
+      })
+    )
+    .catch(err => {
+      res.json({
+        message : "가입된 핸드폰 번호가 없습니다.",
+        result : err
+      })
+    });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body)
-  res.send(req.body)
+
+  models.User.create({
+    username : req.body.username,
+    password : req.body.password,
+    name : req.body.name,
+    phone_number : req.body.phone_number
+  }).then(result => {
+    res.json(result);
+  })
+    .catch(result => {
+      res.json(result);
+    })
 });
 
 module.exports = router;
