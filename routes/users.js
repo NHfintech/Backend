14309a38cd8991198ce2e13b1ba0ba564dc9f1b5
KHS-kr <ks96ks@ajou.ac.kr>
2020-11-22 20:06:39 +0900

    result 사용

--- routes/users.js
+++ routes/users.js
@@ -4,34 +4,39 @@ const {User} = require('../models');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-    await User.findOne({where: {username: req.query.username}})
-        .then((result) =>
-            res.json({
-                message: '이미 가입된 아이디가 있습니다.',
-                result: result,
-            }),
-        )
-        .catch((err) => {
-            res.json({
-                message: '가입된 아이디가 없습니다.',
-                result: err,
-            });
-        });
+    var result = await User.findOne({where: {username: req.query.username}});
+
+    console.dir(result);
+
+    if(result == null) {
+          res.json({
+              message: '가입된 아이디가 없습니다.'
+          });
+    }
+    else {
+          res.json({
+              message: '이미 가입된 아이디가 있습니다.'
+          });
+    }
 });
 
 router.post('/', async function(req, res, next) {
-    await User.create({
-        username: req.body.username,
-        password: req.body.password,
-        name: req.body.name,
-        phone_number: req.body.phone_number,
-    })
-        .then((result) => {
-            res.json(result);
-        })
-        .catch((result) => {
-            res.json(result);
+    try {
+        var result = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+            name: req.body.name,
+            phone_number: req.body.phone_number,
         });
+        res.json({
+          message : '회원가입을 성공하였습니다.'
+        });
+    }
+    catch(exception) {
+        res.json({
+            message : '회원가입 도중 에러가 발생하였습니다.'
+        });
+    }
 });
 
 module.exports = router;
