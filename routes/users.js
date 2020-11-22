341902108582b241a05658f66f6f5762d4fbb0cd
KHS-kr <ks96ks@ajou.ac.kr>
2020-11-22 17:22:13 +0900

    eslint 추가, users.js 수정

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
+const {User} = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  models.User.findOne({where : {username : req.query.username}})
-    .then(result =>
-      res.json({
-        message : "이미 가입된 아이디가 있습니다.",
-        result : result
-      })
-    )
-    .catch(err => {
-      res.json({
-        message : "가입된 핸드폰 아이디가 없습니다.",
-        result : err
-      })
-    });
+router.get('/', async function(req, res, next) {
+    await User.findOne({where: {username: req.query.username}})
+        .then((result) =>
+            res.json({
+                message: '이미 가입된 아이디가 있습니다.',
+                result: result,
+            }),
+        )
+        .catch((err) => {
+            res.json({
+                message: '가입된 아이디가 없습니다.',
+                result: err,
+            });
+        });
 });
 
-router.post('/', function(req, res, next) {
-
-  models.User.create({
-    username : req.body.username,
-    password : req.body.password,
-    name : req.body.name,
-    phone_number : req.body.phone_number
-  }).then(result => {
-    res.json(result);
-  })
-    .catch(result => {
-      res.json(result);
+router.post('/', async function(req, res, next) {
+    await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name,
+        phone_number: req.body.phone_number,
     })
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((result) => {
+            res.json(result);
+        });
 });
 
 module.exports = router;
