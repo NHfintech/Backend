aef264a1b7ba9ebe67d562184d3de3ababe077d0
KHS-kr <ks96ks@ajou.ac.kr>
2020-11-30 13:41:47 +0900

    response json localize, 회원가입된 핸드폰번호가 eventadmin에 있으면 user_id update

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {User} = require('../models');
+const {User, EventAdmin} = require('../models');
 const bcrypt = require('bcrypt');
 const util = require('../utils.js');
-const saltRounds = 10;
 const code = util.code;
-
-const responseJson = {};
 
 /* GET users listing. */
 // 핸드폰번호 중복체크
 // 회원인지 확인하고 문자 아니면 알림 보낼때 사용
 router.get('/overlap/phone', async function(req, res, next) {
+    const responseJson = {};
     if(!util.phoneNumberCheck(req.query.phone_number)) {
         responseJson.result = code.PHONE_NUMBER_INVALID;
         responseJson.detail = 'phone number invalid';
-    } else {
+    }
+    else {
         const result = await User.findOne(
             {where: {phone_number: req.query.phone_number}},
         );
@@ -23,7 +22,8 @@ router.get('/overlap/phone', async function(req, res, next) {
         if(result == null) {
             responseJson.result = code.SUCCESS;
             responseJson.detail = 'no exist';
-        } else {
+        }
+        else {
             responseJson.result = code.PHONE_NUMBER_ALREADY_EXIST;
             responseJson.detail = 'phone number already exist';
         }
@@ -33,6 +33,7 @@ router.get('/overlap/phone', async function(req, res, next) {
 
 // 아이디 중복체크
 router.get('/overlap/username', async function(req, res, next) {
+    const responseJson = {};
     const result = await User.findOne(
         {where: {username: req.query.username}},
     );
@@ -40,7 +41,8 @@ router.get('/overlap/username', async function(req, res, next) {
     if(result == null) {
         responseJson.result = code.SUCCESS;
         responseJson.detail = 'no exist';
-    } else {
+    }
+    else {
         responseJson.result = code.USERNAME_ALREADY_EXIST;
         responseJson.detail = 'username already exist';
     }
@@ -49,24 +51,47 @@ router.get('/overlap/username', async function(req, res, next) {
 
 // 회원가입
 router.post('/signup', async function(req, res, next) {
+    const responseJson = {};
     try {
         if(util.phoneNumberCheck(req.body.phone_number)) {
+            const bcPw = bcrypt.hashSync(req.body.password, util.saltRounds);
             const result = await User.create({
                 username: req.body.username,
-                password: bcrypt.hashSync(req.body.password, saltRounds),
+                password: bcPw,
                 name: req.body.name,
                 phone_number: req.body.phone_number,
             });
             responseJson.result = code.SUCCESS;
             responseJson.detail = 'signup success';
-        } else {
+
+            const userId = result.dataValues.id;
+            const isInvited = await EventAdmin.findOne(
+                {where: {user_phone: req.body.phone_number}},
+            );
+
+            if(isInvited) {
+                await EventAdmin.update(
+                    {
+                        user_id: userId,
+                    },
+                    {
+                        where: {
+                            user_phone: req.body.phone_number,
+                        },
+                    },
+                );
+            }
+        }
+        else {
             responseJson.result = code.PHONE_NUMBER_INVALID;
             responseJson.detail = 'phone number invalid';
         }
-    } catch(exception) {
+    }
+    catch(exception) {
         responseJson.result = code.UNKNOWN_ERROR;
-        responseJson.detail = exception.errors[0].message;
-    } finally {
+        responseJson.detail = 'unknown error';
+    }
+    finally {
         res.json(responseJson);
     }
 });
